d58efe0ecb738e88e7017471a4b51b252e65390a
Lukas Kleppmann <lukas.x.kleppmann@gmnail.com>
2025-10-10 22:50:04 +0200

    deleted js and some css

--- script.js
+++ script.js
@@ -6,26 +6,4 @@ const myImg = [
   "./IMG_folder/Photos/NewYork.jpg",
   "./IMG_folder/Photos/stockholm.jpg",
 ];
-const imgContainer = document.querySelector(".img-container");
-myImg.forEach((src) => {
-  const img = document.createElement("img");
-  img.src = src;
-  img.alt = src.split("/").pop();
-  img.loading = "lazy";
-  imgContainer.appendChild(img);
-});
 
-
-
-let container = document.getElementById("container");
-const modalImage = document.getElementById("modal-image");
-const modal = document.getElementById("modal");
-
-const images = container.querySelectorAll("img");
-
-for (let i = 0; i < image.length; i++) {
-  images[i].addEventListener("click", function () {
-    modalImage.src = images[i].src;
-    modalImage.classList.remove("no-display");
-  });
-}
